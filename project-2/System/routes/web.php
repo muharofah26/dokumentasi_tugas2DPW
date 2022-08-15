0b95ef1ffefbd9a3c2564b9e5663423c0d9fefe8
muharofah26 <muharofah7@gmail.com>
2022-08-15 16:50:54 +0700

    MASUKKAN FOLDER TUGAS 2

---
+++ project-2/System/routes/web.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('index');
+});
+
+
+Route::get('category', function () {
+    return view('category');
+});
+
+
+Route::get('clients', function () {
+    return view('clients');
+});
+
+
+Route::get('contact', function () {
+    return view('contact');
+});
+
+
+Route::get('products', function () {
+    return view('products');
+});
+
+
+Route::get('welcome', function () {
+    return view('welcome');
+});
+
+Route::get('login', function () {
+    return view('login');
+});
